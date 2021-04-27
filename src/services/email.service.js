3d32f2b6bc7c28bee7561d3a840c96bce48d7e67
justin <justin@globaloutliers.com>
2021-04-27 15:22:10 +0900

    also removed send email functions

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -18,10 +18,10 @@ const logger = require('../config/logger');
  * @param {string} text
  * @returns {Promise}
  */
-const sendEmail = async (to, subject, text) => {
-  const msg = { from: config.email.from, to, subject, text };
-  await transport.sendMail(msg);
-};
+// const sendEmail = async (to, subject, text) => {
+//   const msg = { from: config.email.from, to, subject, text };
+//   await transport.sendMail(msg);
+// };
 
 /**
  * Send reset password email
@@ -29,15 +29,15 @@ const sendEmail = async (to, subject, text) => {
  * @param {string} token
  * @returns {Promise}
  */
-const sendResetPasswordEmail = async (to, token) => {
-  const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
-  await sendEmail(to, subject, text);
-};
+// const sendResetPasswordEmail = async (to, token) => {
+//   const subject = 'Reset password';
+//   // replace this url with the link to the reset password page of your front-end app
+//   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+//   const text = `Dear user,
+// To reset your password, click on this link: ${resetPasswordUrl}
+// If you did not request any password resets, then ignore this email.`;
+//   await sendEmail(to, subject, text);
+// };
 
 /**
  * Send verification email
@@ -45,19 +45,19 @@ If you did not request any password resets, then ignore this email.`;
  * @param {string} token
  * @returns {Promise}
  */
-const sendVerificationEmail = async (to, token) => {
-  const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
-  await sendEmail(to, subject, text);
-};
+// const sendVerificationEmail = async (to, token) => {
+//   const subject = 'Email Verification';
+//   // replace this url with the link to the email verification page of your front-end app
+//   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
+//   const text = `Dear user,
+// To verify your email, click on this link: ${verificationEmailUrl}
+// If you did not create an account, then ignore this email.`;
+//   await sendEmail(to, subject, text);
+// };
 
-module.exports = {
-  transport,
-  sendEmail,
-  sendResetPasswordEmail,
-  sendVerificationEmail,
-};
+// module.exports = {
+//   transport,
+//   sendEmail,
+//   sendResetPasswordEmail,
+//   sendVerificationEmail,
+// };
