d050b2dd37c30d32a45568ee6b954af40c596e4a
Justin <outranker222@gmail.com>
2021-05-01 23:38:20 +0900

    added ip address log

--- src/controllers/oxford-api.controller.js
+++ src/controllers/oxford-api.controller.js
@@ -3,7 +3,9 @@ const catchAsync = require('../utils/catchAsync');
 const { oxfordApi } = require('../services');
 
 const getDefinition = catchAsync(async (req, res) => {
-  // console.log(req);
+  const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  console.log('ip address: ', remoteAddress);
   const definitionResult = await oxfordApi.getDefinition(req.query.word);
   if (definitionResult) {
     res.status(httpStatus.CREATED).json({ result: definitionResult.results });
